73f445f281f15c75148251878421d67459c54d4a
Justin Green <justin98366@gmail.com>
2017-04-18 01:45:24 -0700

    started random picture generator

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+
+function randomPics(name){
+  this.name = name;
+  this.pictures = ["img/bag.jpg", "img/banana.jpg", "img/bathroom"
+
+  ];
+  this.path =
+  this.timesShown = timesShown;
+  this.timesClicked = timesClicked;
+
+}
