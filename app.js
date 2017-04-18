41ef6f138a7bd037b0637fcb76e29d66c1e8b3f1
Justin Green <justin98366@gmail.com>
2017-04-18 16:30:19 -0700

    made some more progress

--- app.js
+++ app.js
@@ -1,47 +1,49 @@
 'use strict';
 
-function RandomPics(name, src){
+function RandomPics(name, picSource){
   this.name = name;
-  // this.src = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jgp',
+  // this.src = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg',
   // ];
-  this.src = './assets/' + src;
+  this.picSource = './img/' + picSource;
   //this.path =
   this.timesShown = 0;
   this.timesClicked = 0;
 
 }
-
+var pictureOne = document.getElementById('picture-one');
+var pictureTwo = document.getElementById('picture-two');
+var pictureThree = document.getElementById('picture-three');
 var photosSecond = [];
 var photosOnScreen = [];
 var photosOnPreviousScreen = [];
 var photos = [
-  new RandomPics ('bag', 'bag.jgp'),
-  new RandomPics ('banana', 'banana.jgp'),
-  new RandomPics ('bathroom', 'bathroom.jgp'),
-  new RandomPics ('boots', 'boots.jgp'),
-  new RandomPics ('breakfast', 'breakfast.jgp'),
+  new RandomPics ('bag', 'bag.jpg'),
+  new RandomPics ('banana', 'banana.jpg'),
+  new RandomPics ('bathroom', 'bathroom.jpg'),
+  new RandomPics ('boots', 'boots.jpg'),
+  new RandomPics ('breakfast', 'breakfast.jpg'),
 
-  new RandomPics ('bubblegum', 'bubblegum.jgp'),
-  new RandomPics ('chair', 'chair.jgp'),
-  new RandomPics ('cthulhu', 'cthulhu.jgp'),
-  new RandomPics ('dog duck', 'dog-duck.jgp'),
-  new RandomPics ('dragon', 'dragon.jgp'),
+  new RandomPics ('bubblegum', 'bubblegum.jpg'),
+  new RandomPics ('chair', 'chair.jpg'),
+  new RandomPics ('cthulhu', 'cthulhu.jpg'),
+  new RandomPics ('dog duck', 'dog-duck.jpg'),
+  new RandomPics ('dragon', 'dragon.jpg'),
 
-  new RandomPics ('pen', 'pen.jgp'),
-  new RandomPics ('pet sweeper', 'pet-sweep.jgp'),
-  new RandomPics ('scissors', 'scissors.jgp'),
-  new RandomPics ('shark', 'shark.jgp'),
+  new RandomPics ('pen', 'pen.jpg'),
+  new RandomPics ('pet sweeper', 'pet-sweep.jpg'),
+  new RandomPics ('scissors', 'scissors.jpg'),
+  new RandomPics ('shark', 'shark.jpg'),
   new RandomPics ('sweep', 'sweep.png'),
 
-  new RandomPics ('tauntaun', 'tauntaun.jgp'),
-  new RandomPics ('unicorn', 'unicorn.jgp'),
+  new RandomPics ('tauntaun', 'tauntaun.jpg'),
+  new RandomPics ('unicorn', 'unicorn.jpg'),
   new RandomPics ('usb', 'usb.gif'),
-  new RandomPics ('water can', 'water-can.jgp'),
-  new RandomPics ('wine glass', 'wine-glass.jgp'),
+  new RandomPics ('water can', 'water-can.jpg'),
+  new RandomPics ('wine glass', 'wine-glass.jpg'),
 ];
 
 function getRandomIndex(list){
-  return math.floor(math.random() * list.length);
+  return Math.floor(Math.random() * list.length);
 }
 
 function generatePhotos(){
@@ -62,8 +64,37 @@ function generatePhotos(){
 
   return photosOnScreen;
 }
+generatePhotos();
 
-//photosSecond = photos.concat(photosOnScreen);
+// for (var i = 0; i < 3; i++){
+//   var threePics = document.createElement('img');
+//   threePics.src = photosOnScreen[i].picSource;
+//   pics.appendChild(threePics);
+//   console.log(threePics);
+// }
+var onePics = document.createElement('img');
+onePics.src = photosOnScreen[0].picSource;
+pictureOne.appendChild(onePics);
+
+var twoPics = document.createElement('img');
+twoPics.src = photosOnScreen[1].picSource;
+pictureTwo.appendChild(twoPics);
+
+var threePics = document.createElement('img');
+threePics.src = photosOnScreen[2].picSource;
+pictureThree.appendChild(threePics);
+
+
+var my_div = document.getElementById('picture-one');
+my_div.onclick = function() {
+  console.log('booya!');
+  var clicked = this.timesClicked;
+  clicked.timesClicked++;
+  console.log(clicked.timesClicked);
+
+};
+
+
 
 
 
