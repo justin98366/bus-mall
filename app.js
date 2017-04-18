7d318656ebacca5423b6b4404da0df4bf6185483
Justin Green <justin98366@gmail.com>
2017-04-18 10:22:25 -0700

    modifying code

--- app.js
+++ app.js
@@ -2,8 +2,7 @@
 
 function randomPics(name){
   this.name = name;
-  this.pictures = ["img/bag.jpg", "img/banana.jpg", "img/bathroom"
-
+  this.pictures = ['img/bag.jpg', 'img/banana.jpg', 'img/bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jgp', 
   ];
   this.path =
   this.timesShown = timesShown;
